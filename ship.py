ced4bfb199cd37be554cb66befccdad1c155df63
Lu Weifeng <luwf1992@gmail.com>
2025-06-26 22:50:35 +0800

    weifeng, scale image and code format

--- ship.py
+++ ship.py
@@ -1,13 +1,21 @@
 import pygame
+
 class Ship():
 
    def __init__(self, screen):
       self.screen = screen
-      self.image = pygame.image.load('images/ship.bmp')
-      self.rect = self.image.get_rect()
+      original_image = pygame.image.load('images/ship.bmp')
+
+      # scale the image
+      scale_factor = 0.1
+      self.scaled_image = pygame.transform.scale(original_image, 
+                                                 (original_image.get_width() * scale_factor, 
+                                                  original_image.get_height() * scale_factor))
+
+      self.rect = self.scaled_image.get_rect()
       self.screen_rect = screen.get_rect()
       self.rect.centerx = self.screen_rect.centerx
       self.rect.bottom = self.screen_rect.bottom
 
    def blitme(self):
-       self.screen.blit(self.image, self.rect)
+       self.screen.blit(self.scaled_image, self.rect)
